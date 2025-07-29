aa2685368a7c47bd1689ae8386007388c29e0a4b
Batbilegt <99795941+batbilegt1@users.noreply.github.com>
2025-07-29 09:52:27 +0800

    Update k8s-master-node.sh

--- k8s-master-node.sh
+++ k8s-master-node.sh
@@ -62,7 +62,6 @@ curl -fsSL https://pkgs.k8s.io/core:/stable:/${K8S_VERSION%.*}/deb/Release.key |
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/${K8S_VERSION%.*}/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update
 sudo apt install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
 sudo systemctl enable kubelet
 
 ### Pull Kubernetes images
