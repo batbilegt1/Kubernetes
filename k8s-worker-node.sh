de49fbc85d7116533dc519a14a5cd35ac4985921
Batbilegt <99795941+batbilegt1@users.noreply.github.com>
2025-07-29 09:52:46 +0800

    Update k8s-worker-node.sh

--- k8s-worker-node.sh
+++ k8s-worker-node.sh
@@ -62,7 +62,6 @@ curl -fsSL https://pkgs.k8s.io/core:/stable:/${K8S_VERSION%.*}/deb/Release.key |
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/${K8S_VERSION%.*}/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update
 sudo apt install -y kubelet kubeadm
-sudo apt-mark hold kubelet kubeadm
 sudo systemctl enable kubelet
 
 ### Pull required Kubernetes images
